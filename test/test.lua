f89645ed6a1865db3674e854224898da997b1e7a
opussf <github@zz9-za.com>
2020-08-25 17:32:03 -0700

    New test file
    Never frighten a little man. He'll kill you.

---
+++ test/test.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/env lua
+
+addonData = { ["Version"] = "1.0",
+}
+
+require "wowTest"
+
+test.outFileName = "testOut.xml"
+
+-- Figure out how to parse the XML here, until then....
+HSFrame = CreateFrame()
+
+-- require the file to test
+ParseTOC( "../src/HearthStone.toc" )
+
+
+-- addon setup
+
+function test.before()
+end
+function test.after()
+end
+
+test.run()
