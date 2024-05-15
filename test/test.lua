8664424693706965c2e8b564a7d8007bdd550f3f
opussf <github@zz9-za.com>
2024-05-15 12:30:31 -0700

    New tests, and #hs now ends the line to modify
    <no random message>

--- test/test.lua
+++ test/test.lua
@@ -84,17 +84,25 @@ end
 -- 	HS.Command( "update" )
 -- 	assertEquals( "/use item:165670#hs", HS_settings.macro[3] )
 -- end
--- function test.test_macroSize()
--- 	assertEquals( 30, string.len( myMacros.general[1].text ))
--- end
-function test.test_long_macro()
-	test.dump( HS_settings )
-	EditMacro( "testmacro", nil, nil, "1234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n#hs\n/use 1\nHello")
-
-	HS.Command( "update" )
-	test.dump(myMacros)
-	test.dump(HS_log)
+function test.test_macroSize()
 	assertEquals( 30, string.len( myMacros.general[1].text ))
+end
+function test.test_long_macro_old_method_does_not_destroy()
+	-- test.dump( HS_settings )
+	EditMacro( "testmacro", nil, nil, "1234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n#hs\n/use item:1234\n123456789012345678901234567890\n2345")
+	HS.Command( "update" )
+	-- test.dump(myMacros)
+	-- test.dump(HS_log)
+	assertEquals( 255, string.len( myMacros.general[1].text ) )
+	assertTrue( string.find( myMacros.general[1].text, "/use item:1234" ))
+end
+function test.test_long_macro_post_edit_works_ok()
+	EditMacro( "testmacro", nil, nil, "1234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n234567890123456789012345678901234567890\n/use item:4321#hs\n123456789012345678901234567890\n12345")
+	HS.Command( "update" )
+	-- test.dump(myMacros)
+	-- test.dump(HS_log)
+	assertEquals( 255, string.len( myMacros.general[1].text ) )
+	assertTrue( string.find( myMacros.general[1].text, "/use item:6948" ))
 end
 
 test.run()
