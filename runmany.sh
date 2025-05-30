be71b2b6d15039457ea29aedde8764576b8e1bb6
opussf <github@zz9-za.com>
2025-05-30 12:00:43 -0700

    save before delete
    When I read about the evils of drinking, I gave up reading.

--- runmany.sh
+++ runmany.sh
@@ -4,7 +4,7 @@ mkdir -p target/reports
 for n in $(seq -f "%05g" 9999 1) ; do
 	#echo "Running run $n"
 	reportFile="target/reports/testOut.xml"
-	ant test > target/reports/antout.txt
+	ant wow > target/reports/antout.txt
 	if [ ! "$?" == "0" ]; then
 		cp target/reports/antout.txt target/reports/antOut$n.txt
 		#mv $reportFile target/reports/testOut$n.xml
